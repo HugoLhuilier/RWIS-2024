99e6f36830620bc483ef922625118c3893a584db
Hugo <115175312+HugoLhuilier@users.noreply.github.com>
2024-12-17 17:12:23 +0900

    Falling animation
    Animation added when falling

--- Assets/Scripts/Entities/Comp/CanFall.cs
+++ Assets/Scripts/Entities/Comp/CanFall.cs
@@ -6,19 +6,47 @@ using UnityEngine;
 [RequireComponent(typeof(Entity))]
 public class CanFall : MonoBehaviour
 {
+    [SerializeField] private float fallingTime = 1.5f;
+    [SerializeField] private float stoppingTime = 0.2f; // Time is seconds needed to stop the movement of the player from the moment it's falling
+
     private Rigidbody2D rb;
     private bool isFalling = false;
+    private float timeSinceFall = 0;
     private Entity ent;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 finalVel;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         ent = GetComponent<Entity>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void fall()
+    private void Update()
     {
-        ent.Die();
+        if (isFalling)
+        {
+            timeSinceFall += Time.deltaTime;
+
+            if (timeSinceFall > fallingTime)
+            {
+                ent.Die();
+            }
+
+
+            float frac = 1 - timeSinceFall / fallingTime;
+            transform.localScale = frac * Vector3.one;
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.a, frac);
+
+            rb.velocity = Mathf.Clamp01(1 - timeSinceFall / stoppingTime) * finalVel;
+        }
+    }
+
+    public void startFalling()
+    {
+        finalVel = rb.velocity;
+        rb.isKinematic = true;
         isFalling = true;
     }
 
@@ -33,7 +61,7 @@ public class CanFall : MonoBehaviour
         {
             if (collision.OverlapPoint(transform.TransformPoint(rb.centerOfMass)))
             {
-                fall();
+                startFalling();
             }
         }
     }
