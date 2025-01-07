5d8d245054e18daa6f8e390bf9fa64ab86ffd3d2
Hugo <115175312+HugoLhuilier@users.noreply.github.com>
2025-01-07 11:36:03 +0900

    Timer
    Addinga timer measuring the game completion time

--- Assets/Scripts/Misc/GameManager.cs
+++ Assets/Scripts/Misc/GameManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
+using TMPro;
 
+[RequireComponent(typeof(Timer))]
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject winScreen;
     [SerializeField] private GameObject loseScreen;
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    private Timer timer;
 
     public UnityEvent pauseTime;
     public UnityEvent resumeTime;
@@ -17,40 +22,50 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        timer = GetComponent<Timer>();
+
         Time.timeScale = 1.0f;
+
+        StartGame();
+    }
+
+    private void Update()
+    {
+        timerText.text = timer.getTime().ToString();
     }
 
     public void Win()
     {
+        timer.stopTimer();
         Time.timeScale = 0;
         winScreen.SetActive(true);
     }
 
     public void Lose()
     {
+        timer.stopTimer();
         Time.timeScale = 0;
         loseScreen.SetActive(true);
     }
 
     public void Restart()
     {
-        Debug.Log("Restart");
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        timer.startTimer();
     }
 
     public void PauseTime()
     {
-        Debug.Log("Time paused");
-
         timePaused = true;
         pauseTime.Invoke();
     }
 
     public void ResumeTime()
     {
-        Debug.Log("Time paused");
-
         timePaused = false;
         resumeTime.Invoke();
     }
